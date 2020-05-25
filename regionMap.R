15c6c186e4570a36c92785f8f1ee3c281ab88256
Hannah <direch@uw.edu>
2020-05-25 14:22:50 -0700

    sep non-reg ocean from not fit regions

--- regionMap.R
+++ regionMap.R
@@ -8,16 +8,40 @@ lineCols <- c("darkblue", "purple4", "darkgreen",
 regCols <- c("dodgerblue1", "mediumpurple1", "lightgreen",
              "lightpink", "coral")
 
+
+#Read in NSIDC regions
+fileName <- "/Users/hdirector/Dropbox/SeaIce_InProgress/probContours_ECMWF/Data/grids/region_n.msk"
+to.read <- file(fileName, "rb")
+nX <- 304; nY <- 448
+to.read = file(fileName, "rb")
+skip <- readBin(to.read, "raw", 300) #skip 300 byte header
+dat <- readBin(to.read, "int", size = 1, signed = FALSE, n = nX*nY)
+close(to.read)
+reg <- matrix(dat, nrow = nX)
+reg <- reg[, nY:1]
+non_reg <- matrix(nrow = nX, ncol = nY, data = 0)
+non_reg[reg == 1] <- 1
+non_reg <- get_region(non_reg, dat_type = "simple", use_all = TRUE)
+xmn = -3850; xmx = 3750; ymn = -5350; ymx = 5850 #min and max
+bbPts <- rbind(c(xmn, ymn), c(xmn, ymx), c(xmx, ymx), c(xmx, ymn))
+bb <- SpatialPolygons(list(Polygons(list(Polygon(bbPts)), "box")))
+
+
 #make map
-plot(land, col = 'grey', border = "grey")
+plot(bb, col = 'beige')
 for (i in 1:nReg) {
   plot(reg_info$regions[[i]], add = T, col = regCols[i], border = regCols[i])
 } 
+plot(non_reg, col = "white", add = T, border = "white")
+plot(land, add = T, col = 'grey', border = 'grey')
+plot(bb, border = 'white', add = T)
 plot(reg_info$start_lines[[1]], add = T, col = lineCols[1],
      lwd = 2)
 for (i in 2:nReg) {
   plot(reg_info$start_lines[[i]], add = T, col = lineCols[i])
 }
+
+
 
 text(1000, 2800, "Russia", font = 2, cex = .65, col= 'navy')
 text(-2700, 1000, "Alaska", font = 2, cex = .65, col= 'navy')
@@ -26,4 +50,5 @@ text(-500, 500, "Central Arctic", font = 2, cex = .65)
 text(-2700, 2300, "Bering Sea", font = 2, cex = .65)
 text(-600, -3000, "Baffin \n Bay", font = 2, cex = .65)
 text(1250, -1500, "Greenland \n Sea", font = 2, cex = .65)
+
 dev.off()
