21f6d6230c1639ca2030ba3538592ad2dccd1b6b
Hannah <direch@uw.edu>
2020-05-27 14:37:01 -0700

    adding label to Sea of Okhotsk

--- regionMap.R
+++ regionMap.R
@@ -1,5 +1,6 @@
+library("IceCast")
 pdf("Paper/Figures/regionMap.pdf")
-xBdInd <- 55:225; xBdN <- length(xBdInd) 
+xBdInd <- 55:225; xBdN <- length(xBdInd)
 yBdInd <- 130:310; yBdN <- length(yBdInd)
 par(oma = rep(0, 4), mar = rep(0, 4))
 #colors
@@ -28,20 +29,19 @@ bb <- SpatialPolygons(list(Polygons(list(Polygon(bbPts)), "box")))
 
 
 #make map
+nReg <- length(reg_info$regions)
 plot(bb, col = 'beige')
 for (i in 1:nReg) {
   plot(reg_info$regions[[i]], add = T, col = regCols[i], border = regCols[i])
-} 
+}
 plot(non_reg, col = "white", add = T, border = "white")
 plot(land, add = T, col = 'grey', border = 'grey')
 plot(bb, border = 'white', add = T)
 plot(reg_info$start_lines[[1]], add = T, col = lineCols[1],
      lwd = 2)
 for (i in 2:nReg) {
-  plot(reg_info$start_lines[[i]], add = T, col = lineCols[i])
+  points(reg_info$start_lines_coords[[i]], type= 'l', col = lineCols[i])
 }
-
-
 
 text(1000, 2800, "Russia", font = 2, cex = .65, col= 'navy')
 text(-2700, 1000, "Alaska", font = 2, cex = .65, col= 'navy')
@@ -50,5 +50,6 @@ text(-500, 500, "Central Arctic", font = 2, cex = .65)
 text(-2700, 2300, "Bering Sea", font = 2, cex = .65)
 text(-600, -3000, "Baffin \n Bay", font = 2, cex = .65)
 text(1250, -1500, "Greenland \n Sea", font = 2, cex = .65)
+text(-950, 4000, "Sea of \n Okhotsk", font = 2, cex = .65)
 
 dev.off()
